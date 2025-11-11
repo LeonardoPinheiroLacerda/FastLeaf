115118de91a46f33220d9070d7e607a722a62251
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2025-11-10 23:29:24 -0300

    test: unit tests for annotation package

---
+++ src/test/java/br/com/leonardo/annotation/scanner/Middleware1.java
@@ -0,0 +1,12 @@
+package br.com.leonardo.annotation.scanner;
+
+import br.com.leonardo.exception.HttpMiddlewareException;
+import br.com.leonardo.http.middleware.Middleware;
+import br.com.leonardo.http.request.HttpRequest;
+
+public class Middleware1 extends Middleware {
+    @Override
+    public void run(HttpRequest<?> request) throws HttpMiddlewareException {
+        System.out.println("eu sou um middleware :)");
+    }
+}
